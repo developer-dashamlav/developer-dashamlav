5fc42fca546f9fa90608be4165b01ce0b1326d4c
prajwal257 <69119435+prajwal257@users.noreply.github.com>
2023-11-08 15:06:10 +0530

    update dated 8 Nov 2023

---
+++ services.js
@@ -0,0 +1,60 @@
+services = {
+    "service": {
+        "Online Marketplace Optimization": [
+            "Product Listing Optimization",
+            "Inventory MAnagement",
+            "Competitive Analysis",
+            "Pricing Stretegy",
+            "Review and Rating Management"
+        ],
+        "E-Commerce Website Developement": [
+            "Website Design and Development",
+            "User Experience Optimization",
+            "Mobile Responsiveness",
+            "Security and Payment Gateway"
+        ],
+        "Social Media Marketing": [
+            "Social Media Stretegy Development",
+            "Content Creation and Posting",
+            "Community Engagement",
+            "Performance Analytics"
+        ],
+        "Content Marketing": [
+            "Blog Post Creation",
+            "Video Content Production",
+            "Infographics and Visuals",
+            "Content Distribution Strategies"
+        ],        
+        "Search Engine Optimization": [
+            "Keyword Reseach and Optimization",
+            "On-Page and Off-Page SEO",
+            "Link Building Stergies",
+            "Local SEO for Physical Stores"
+        ],
+        "Paid Advertising Mangement": [
+            "Google Ads and Social Media Advertising",
+            "Ad campain and Optimization",
+            "AB Testing",
+            "Conversion tracking and Reporting"
+        ],
+        "Email Marketing": [
+            "Email Campaign Strategy",
+            "Subscriber List Management",
+            "Email Design and Content Creation",
+            "Personalization and Segmentation",
+            "Performance Tracking and Analysis"
+        ],
+        "Analytics and Reporting": [
+            "Data Analysis and Interpretation",
+            "Cusomized Performance Reports",
+            "Goal Tracking and KPI Management",
+            "Recommendations for Improvements"
+        ],
+        "Branding and Design": [
+            ""
+        ],
+        "360° E-Commerce Strategy and Consultation": {
+
+        }
+    }
+}
